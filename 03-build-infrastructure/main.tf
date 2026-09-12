b440ca9a6ce6ae7c4e8c82330adf8bc1dfc03497
Soham More <sohamdi2301@gmail.com>
2026-09-13 01:48:25 +0530

    Add files via upload

---
+++ 03-build-infrastructure/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+data "aws_ami" "amazon_linux_2023" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023.*-x86_64"]
+  }
+}
+
+resource "aws_instance" "app_server" {
+  ami           = data.aws_ami.amazon_linux_2023.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "Build-Infrastructure-Instance"
+  }
+}
